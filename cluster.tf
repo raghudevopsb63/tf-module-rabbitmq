f6f2ac7cafbb85d153e68780676122dc6730420f
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-02 06:57:28 +0530

    Update terraform/tf-module-rabbitmq/

--- cluster.tf
+++ cluster.tf
@@ -27,11 +27,6 @@ resource "aws_security_group" "allow_rabbitmq" {
 resource "aws_mq_broker" "rabbitmq" {
   broker_name = "roboshop-${var.ENV}"
 
-  //  configuration {
-  //    id       = aws_mq_configuration.config-main.id
-  //    revision = aws_mq_configuration.config-main.latest_revision
-  //  }
-
   engine_type        = "RabbitMQ"
   engine_version     = var.RABBITMQ_ENGINE_VERSION
   host_instance_type = var.RABBITMQ_INSTANCE_TYPE
@@ -39,18 +34,8 @@ resource "aws_mq_broker" "rabbitmq" {
   subnet_ids         = [data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS[0]]
 
   user {
-    username = "roboshop"
-    password = "RoboShop1234"
+    username = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["RABBITMQ_USERNAME"]
+    password = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["RABBITMQ_PASSWORD"]
   }
 }
-
-//resource "aws_mq_configuration" "config-main" {
-//  description             = "roboshop-${var.ENV}"
-//  name                    = "roboshop-${var.ENV}"
-//  engine_type             = "RabbitMQ"
-//  engine_version          = "3.9.13"
-//  data                    = ""
-//  authentication_strategy = "simple"
-//}
-
 
--- data.tf
+++ data.tf
@@ -7,3 +7,13 @@ data "terraform_remote_state" "vpc" {
   }
 }
 
+
+data "aws_secretsmanager_secret" "secrets" {
+  name = "${var.ENV}/roboshop/secrets"
+}
+
+data "aws_secretsmanager_secret_version" "secrets" {
+  secret_id = data.aws_secretsmanager_secret.secrets.id
+}
+
+
