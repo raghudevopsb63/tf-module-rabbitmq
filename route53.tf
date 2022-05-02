abe65d1d4bc411ece26cc580e7f15c660c628c28
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-02 06:19:13 +0530

    Update terraform/tf-module-rabbitmq/

--- route53.tf
+++ route53.tf
@@ -1,7 +1,7 @@
-//resource "aws_route53_record" "record" {
-//  zone_id = data.terraform_remote_state.vpc.outputs.HOSTEDZONE_PRIVATE_ID
-//  name    = "rabbitmq-${var.ENV}.${data.terraform_remote_state.vpc.outputs.HOSTEDZONE_PRIVATE_ZONE}"
-//  type    = "CNAME"
-//  ttl     = "300"
-//  records = [aws_mq_broker.rabbitmq.]
-//}
+resource "aws_route53_record" "record" {
+  zone_id = data.terraform_remote_state.vpc.outputs.HOSTEDZONE_PRIVATE_ID
+  name    = "rabbitmq-${var.ENV}.${data.terraform_remote_state.vpc.outputs.HOSTEDZONE_PRIVATE_ZONE}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["${aws_mq_broker.rabbitmq.id}.mq.us-east-1.amazonaws.com"]
+}
